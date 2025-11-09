275e5bcbc8552ae03bf088a8b976e3af0ae55c5d
Mr adhil <ekadhilcom@gmail.com>
2025-11-09 16:17:50 +0530

    frist part that done with index page and base

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request,redirect
+import smtplib
+
+
+app=Flask(__name__,template_folder='Templates',static_folder='static')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+@app.route('/project')
+def project():
+    return render_template('project.html')
+@app.route('/services')
+def services():
+    return render_template('services.html')
+@app.route('/team')
+def team():
+    return render_template('team.html')
+if __name__ == "__main__":
+    app.run(debug=True,port=5050)
